b820c5f19f17c85ca7e7f9c9d7b8a1bd46be0662
yonaiky <56011382+yonaiky@users.noreply.github.com>
2023-08-17 16:01:58 -0400

    x
    x

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import styled from '@emotion/styled'
 import ImagenCripto from './img/imagen-criptos.png'
+import Formulario from './Components/Formulario'
 
 const Contenedor = styled.div`
 max-width: 900px;
 margin: 0 auto;
-width 90%;
+width:90%;
 @media (min-width: 992px){
 	display: grid;
 	grid-template-columns: repeat(2,1fr);
@@ -22,9 +23,21 @@ display:block;
 const Heading = styled.h1`
 font-family: 'Lato', sans-serif;
 color: #FFF;
+text-align: center;
+font-weight: 700;
+margin-top: 80px;
+margin-bottom: 50px;
+font-size: 34px;
+
+&::after{
+	content: '';
+	width: 100px;
+	height: 6px;
+	background-color:#427fdb ;
+	display: block;
+	margin : 10px auto 0 auto;
+}
 `
-
-
 function App() {
 
   return (
@@ -34,7 +47,10 @@ function App() {
 		src={ImagenCripto}
 		alt="Imagen criptomonedas" />
 
-	<Heading>Hello Word</Heading>
+	<div>
+	<Heading>Cotiza Criptomonedas al instante</Heading>
+	<Formulario />
+	</div>
 
 	</Contenedor>
      
